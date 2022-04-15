282bb7ca2d90f1ed655a0df80164bd984192b0de
leerivera <leerivera80@gmail.com>
2022-04-15 11:50:11 -0400

    again

---
+++ apistuff/objects-shots/js/main.js
@@ -0,0 +1,28 @@
+//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+document.querySelector('button').addEventListener('click', getDrink)
+
+function getDrink(){
+    let drink = document.querySelector('input').value
+
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data.drinks)
+      // document.querySelector('h2').innerText = data.drinks[0].strDrink
+      data.drinks.forEach( el => {
+        console.log(el)
+        const li = document.createElement('li')
+        //add text to li
+        li.textContent = el.strDrink
+        //append the li to the ul
+        document.querySelector('ul').appendChild(li)
+      })
+      document.querySelector('img').src = data.drinks[0].strDrinkThumb
+      document.querySelector('h3').innerText = data.drinks[0].strInstructions
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+
+}
+
